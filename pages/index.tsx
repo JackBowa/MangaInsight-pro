f2b32ad3d6135a5165b4c1b467db0be7016cdf03
JackBowa <167618893+JackBowa@users.noreply.github.com>
2025-09-21 15:54:43 +0200

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'system-ui' }}>
-      <h1 style={{ fontSize: '2rem' }}>Hello Mangainsight 👋</h1>
-      <p>Ça marche !</p>
-    </div>
-  );
-}
