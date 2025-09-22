a4d856ee47b4459b2ebe96a93c36fde5ea7deb7d
JackBowa <167618893+JackBowa@users.noreply.github.com>
2025-09-22 12:24:28 +0200

    Update Comments.tsx

--- app/series/[slug]/Comments.tsx
+++ app/series/[slug]/Comments.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useMemo, useState } from "react";
-import { supabase } from "@/lib/supabase/client";
+import { supabase } from "@/lib/lib/supabase/client";
 
 type DbComment = {
   id: string;
